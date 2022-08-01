f79591e0577f5957357e4bd8da6d3a63e8cdcb07
bekpulatovpro <jbekpulatov@yandex.ru>
2022-08-01 14:13:18 +0500

    added new functional elements

--- src/Pages/Films/Films.jsx
+++ src/Pages/Films/Films.jsx
@@ -1,45 +1,57 @@
 import React from 'react'
 import "./Films.css"
-import { useEffect, useState } from "react"
+import { useEffect, useState, useRef } from "react"
 import axios from "axios"
-import {NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 
 
 function Films() {
     const [api, setApi] = useState([])
-    const [loading,setLoading]=useState(false)
-    const getApi = () => {
-        setLoading(true)
-        axios.get("https://api.cinerama.uz/v2/web/content/movies/list?items=300&module_id=3")
+
+    const [count, setCount] = useState(30)
+    const getApi = (count) => {
+        axios.get(`https://api.cinerama.uz/v2/web/content/movies/list?items=${count}&module_id=3`)
             .then(response => {
                 const myApi = response.data.data.movies
                 setApi(myApi)
-                setLoading(false)
+
 
             })
     }
 
-    
-    useEffect(() => getApi(), [])
+    useEffect(() => {
+
+    })
+    useEffect(() => getApi(count), [count])
+    const usingButton = () => {
+        setCount(p => p + 60)
+
+
+    }
     return (
         <div className='film'>
             <div className='container'>
-                <h1 className='film__title'>Фильмы</h1>
-                
-                    <ul className='film__list'>
+
+                <h1 className='film__title'>
+
+                    Фильмы</h1>
+
+                <ul className='film__list'>
                     {
                         api.map((element) => (
-                           <NavLink className="title" to={`/film/${element.id}`}>
-                             <li className='film__item' key={element.id}>
+                            <NavLink className="title" to={`/film/${element.id}`}>
+                                <li className='film__item' key={element.id}>
                                     <img className='film__image' src={element.poster} alt={element.titleEn} />
                                     <span className='span'></span>
                                     <p className='film__text'>{element.title}</p>
                                 </li>
-                           </NavLink>
+                            </NavLink>
                         ))
                     }
-                    </ul>
-                
+                </ul>
+
+                <button onClick={usingButton} className='film__btn'>Показать больше</button>
+
             </div>
         </div>
     )
--- src/Pages/SinglePage/SinglePage.jsx
+++ src/Pages/SinglePage/SinglePage.jsx
@@ -1,33 +1,45 @@
 import React from 'react'
-import {useParams} from "react-router-dom"
-import {useState,useEffect} from "react"
-// import axios from "axios"
+import { useParams } from "react-router-dom"
+import { useState, useEffect } from "react"
+import axios from "axios"
+import "./SinglePage.css"
 
 function SinglePage() {
-    const {id}=useParams()
-    const [post,setPost]=useState([])
-    useEffect(()=>{
-        fetch(`https://api.cinerama.uz/v2/web/content/movies/list?items=30&module_id=3/${id}`)
-        .then(res=>res.json())
-        .then(data=>setPost(data.data.movies))
-    },[id])
+
+    const { id } = useParams()
+
+    const [post, setPost] = useState([])
+
+    useEffect(() => {
+        axios
+            .get(`https://api.cinerama.uz/v2/web/content/movie/movie-files?id=${id}&module_id=3`)
+            .then(total => {
+                setPost(total.data.data)
+            })
+    }, [id])
     console.log(post);
     return (
-        <div>
-          
-          {post.map(a=>(
-            <>
-            
-            <h1>{a.title}</h1>
-            </>
-          ))
-          
-          }
-            
-            
+        <div >
+            {post.map((element) => (
+                <>
+                    <img className='params__image' src={element.image} alt={element.title} />
+
+                    <span className='params__span'></span>
+                </>
+            ))
+            }
+
+
         </div>
-        
     )
+
+
+
 }
 
 export default SinglePage
+
+
+
+
+
